325e20149c74c381607ac4b8c676b77dfef32d13
LeonMarchetti <mleonardoa@gmail.com>
2018-04-19 08:40:45 -0300

    Clases e interfaces completadas.

--- src/modelo/cargo/GestorCargo.java
+++ src/modelo/cargo/GestorCargo.java
@@ -1,6 +1,33 @@
 package modelo.cargo;
 
+import java.util.ArrayList;
+
+import modelo.auxiliares.EstadoOperacion;
 
 public class GestorCargo {
+    public EstadoOperacion nuevoCargo(ICargo cargo) {
+        // TODO actualizar BD
+        return new EstadoOperacion(EstadoOperacion.CodigoEstado.INSERT_OK,
+                "El cargo se creó correctamente.");
+    }
 
+    public EstadoOperacion modificarCargo(ICargo cargo) {
+        // TODO actualizar BD
+        return new EstadoOperacion(EstadoOperacion.CodigoEstado.UPDATE_OK,
+                "El cargo se modificó correctamente");
+    }
+
+    public EstadoOperacion eliminarCargo(ICargo cargo) {
+        // TODO actualizar BD
+        return new EstadoOperacion(EstadoOperacion.CodigoEstado.DELETE_OK,
+                "El cargo se eliminó correctamente");
+    }
+
+    public ArrayList<ICargo> listarCargo(ICargo cargo) {
+        if (cargo != null) {
+            // TODO Filtrar por los campos que ingresan
+        }
+        // TODO select BD
+        return null;
+    }
 }
--- src/modelo/cargo/ICargo.java
+++ src/modelo/cargo/ICargo.java
@@ -2,5 +2,11 @@ package modelo.cargo;
 
 
 public interface ICargo {
-
+    public ICargo clone();
+    public int getCodigo();
+    public void setCodigo(int codigo);
+    public String getDescripcion();
+    public void setDescripcion(String descripcion);
+    public int getCargaHoraria();
+    public void setCargaHoraria(int cargaHoraria);
 }
