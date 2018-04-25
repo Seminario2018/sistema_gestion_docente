f394c6f0f80231c2891bf031dc6dddad74e33c33
Tomás <tomasjuran96@gmail.com>
2018-04-25 14:54:22 -0300

    Finalizado armarConsulta en TipoInforme

--- src/modelo/informe/ColumnaInforme.java
+++ src/modelo/informe/ColumnaInforme.java
@@ -1,15 +1,27 @@
 package modelo.informe;
 
+import java.util.List;
+
 public class ColumnaInforme {
 	public static final int SIN_ORDEN = 0;
 	public static final int ASCENDENTE = 1;
 	public static final int DESCENDENTE = 2;
 	
 	private String atributo; // PLANTA.ULTIMO_COSTO
-	private FiltroColumna filtro; // >= 2017
-	private CalculoColumna calculo; // COUNT()
+	private List<String> filtros; // >= 2017
+	private String calculo; // COUNT
 //	GrupoColumna grupo;
-	private int ordenar;
+	private int ordenar; // SIN_ORDEN, ASCENDENTE O DESCENDENTE
+	private int posicion; // Posición de la columna en el informe
+	
+	public ColumnaInforme(String atributo, List<String> filtros, String calculo, int ordenar, int posicion) {
+		super();
+		this.atributo = atributo;
+		this.filtros = filtros;
+		this.calculo = calculo;
+		this.ordenar = ordenar;
+		this.posicion = posicion;
+	}
 	
 	public String getAtributo() {
 		return atributo;
@@ -17,22 +29,28 @@ public class ColumnaInforme {
 	public void setAtributo(String atributo) {
 		this.atributo = atributo;
 	}
-	public FiltroColumna getFiltro() {
-		return filtro;
+	public List<String> getFiltros() {
+		return filtros;
 	}
-	public void setFiltro(FiltroColumna filtro) {
-		this.filtro = filtro;
+	public void setFiltros(List<String> filtros) {
+		this.filtros = filtros;
 	}
-	public CalculoColumna getCalculo() {
+	public String getCalculo() {
 		return calculo;
 	}
-	public void setCalculo(CalculoColumna calculo) {
+	public void setCalculo(String calculo) {
 		this.calculo = calculo;
 	}
 	public int getOrdenar() {
 		return ordenar;
 	}
-	public void setOrdenar(int orden) {
-		this.ordenar = orden;
+	public void setOrdenar(int ordenar) {
+		this.ordenar = ordenar;
+	}
+	public int getPosicion() {
+		return posicion;
+	}
+	public void setPosicion(int posicion) {
+		this.posicion = posicion;
 	}
 }
--- src/modelo/informe/TipoInforme.java
+++ src/modelo/informe/TipoInforme.java
@@ -7,47 +7,72 @@ public class TipoInforme implements ITipoInforme {
 	// Estos atributos se levantan de la BD
 	private List<ColumnaInforme> columnas;
 	private String from; // solo from con joins
+	private String groupby; // groupby prearmado
 
-	public List<List<String>> armarConsulta() {
+	public TipoInforme(List<ColumnaInforme> columnas, String from, String groupby) {
+		super();
+		this.columnas = columnas;
+		this.from = from;
+		this.groupby = groupby;
+	}
+
+	public String armarConsulta() {
 		String select = "SELECT";
 		String where = "\r\nWHERE";
-		String groupby = "\r\nGROUP BY";
 		String orderby = "\r\nORDER BY";
+		boolean first_select = true;
 		boolean b_where = false;
-		boolean b_groupby = false;
 		boolean b_orderby = false;
 
 		for (ColumnaInforme columna: columnas) {
 
+			String atributo = columna.getAtributo();
+			
 			// Armar el SELECT
+			if (first_select) {
+				select += " ";
+				first_select = false;
+			} else {
+				select += ", ";
+			}
+			
 			if (columna.getCalculo() != null) {
 				// SUM(PLANTA.ULTIMO_COSTO)
-				select += " " + columna.getCalculo().getOperacion() + "("
-						+ columna.getAtributo() + ")";
+				select += columna.getCalculo() + "(" + atributo + ")";
 			} else {
 				// PLANTA.ULTIMO_COSTO
-				select += " " + columna.getAtributo();
+				select += atributo;
 			}
 
-			// Armar el WHERE
-			if (columna.getFiltro() != null) {
-				b_where = true;
-				// DOCENTE.LEGAJO > 140000
-				where += " " +columna.getAtributo()
-						+ columna.getFiltro().getCondicion();
-			}
-
-			// Armar el GROUP BY
-			if (columna.getCalculo() != null) {
-				b_groupby = true;
-				// GROUPBY DOCENTE.LEGAJO
-				groupby += " " + columna.getAtributo();
+			// Armar el WHERE			
+			if (columna.getFiltros() != null 
+					&& !columna.getFiltros().isEmpty()) {
+				
+				int i = 0;
+				List<String> filtros = columna.getFiltros();
+				
+				if (!b_where) {
+					// DOCENTE.LEGAJO > 140000
+					where += " " + atributo + " " + filtros.get(i);
+					b_where = true;
+					i++;
+				}
+				
+				while(i < filtros.size()) {
+					where += " AND " + atributo + filtros.get(i);
+					i++;
+				}
 			}
 
 			// Armar el ORDER BY
 			if (columna.getOrdenar() != ColumnaInforme.SIN_ORDEN) {
-				b_orderby = true;
-				orderby += " " + columna.getAtributo();
+				if (!b_orderby) {
+					b_orderby = true;
+					orderby += " " + atributo;
+				} else {
+					orderby += ", " + atributo;
+				}
+				
 				switch (columna.getOrdenar()) {
 				case ColumnaInforme.ASCENDENTE:
 					// ORDERBY DOCENTE.LEGAJO ASC
@@ -63,20 +88,19 @@ public class TipoInforme implements ITipoInforme {
 			}
 		}
 
-		String consulta = select + this.from;
+		String consulta = select + "\r\nFROM " + this.from;
+		
 		if (b_where) {
 			consulta += where;
 		}
-		if (b_groupby) {
-			consulta += groupby;
-		}
+		
+		consulta += "\r\nGROUPBY " + this.groupby;
+		
 		if (b_orderby) {
 			consulta += orderby;
 		}
-
-		// TODO enviar consulta a la BD
-
-		return null;
+		
+		return consulta;
 	}
 	/*
 	SELECT DOCENTE.LEGAJO, DOCENTE.APYNOM, SUM(PLANTA.ULTIMO_COSTO)
