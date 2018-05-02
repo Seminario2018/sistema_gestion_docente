043f1fc3b85e92bfc2a60a5d4526c6415cff938e
Tomás <tomasjuran96@gmail.com>
2018-05-01 22:00:41 -0300

    Ejemplo controlador para FXML

---
+++ src/main/java/vista/Docentes.java
@@ -0,0 +1,27 @@
+package vista;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ * @author Martín Tomás Juran
+ * @version 1.0, 1 de may. de 2018
+ */
+public class Docentes {
+	
+	/* Pestaña Cargos */
+	@FXML
+	private Button btnCargosGuardar;
+	
+	@FXML
+	public void agregarCargo() {
+		
+	}
+	
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+	}
+}
