032b64ae01786c4dc52ac3db6ea63b8952dbe950
LeonMarchetti <mleonardoa@gmail.com>
2018-05-05 17:51:18 -0300

    Métodos get y set.

--- src/main/java/modelo/auxiliares/EstadoCargo.java
+++ src/main/java/modelo/auxiliares/EstadoCargo.java
@@ -7,14 +7,30 @@ import java.util.ArrayList;
  * @version 1.0, 17 de abr. de 2018
  */
 public class EstadoCargo {
-	
+
 	private int id;
 	private String descripcion;
-	
+
 	/**
 	 * @return la lista de estados de los cargos de la BD
 	 */
 	public static ArrayList<EstadoCargo> getLista() {
 		return null;
 	}
+
+	public int getId() {
+	    return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+	    return this.descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
--- src/main/java/modelo/auxiliares/TipoCargo.java
+++ src/main/java/modelo/auxiliares/TipoCargo.java
@@ -7,14 +7,30 @@ import java.util.ArrayList;
  * @version 1.0, 17 de abr. de 2018
  */
 public class TipoCargo {
-	
+
 	private int id;
 	private String descripcion;
-	
+
 	/**
 	 * @return la lista de estados del docente de la BD
 	 */
 	public static ArrayList<TipoCargo> getLista() {
 		return null;
 	}
+
+	public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
