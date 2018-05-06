4baa1df0b26bf63b2a2e92a158041606d6a9f648
LeonMarchetti <mleonardoa@gmail.com>
2018-05-06 00:16:46 -0300

    Clase de notificación de mail.

---
+++ src/main/java/mail/NotificacionCargo.java
@@ -0,0 +1,54 @@
+package mail;
+
+import java.util.List;
+
+import modelo.docente.ICargoDocente;
+import modelo.docente.IDocente;
+import modelo.persona.IContacto;
+
+public class NotificacionCargo {
+    private static IMail mail = new Mail();
+
+    public static void notificar(IDocente docente, ICargoDocente cargoDocente) {
+        String asunto = "Actualización de cargo";
+        String mensaje = String.format(
+                "Se actualizó el siguiente cargo para el docente:"
+                + "\tLegajo: %d\n"
+                + "\tApellido: %s\n"
+                + "\tNombre: %s\n"
+                + "el siguiente cargo:"
+                + "\tCargo: %s\n"
+                + "\tÁrea: %d\n"
+                + "\tDivisión: %d\n"
+                + "\tTipo de cargo: %s\n"
+                + "\tEstado de cargo: %s\n",
+                docente.getLegajo(),
+                docente.getPersona().getApellido(),
+                docente.getPersona().getNombre(),
+                cargoDocente.getCargo().getDescripcion(),
+                cargoDocente.getArea().getCodigo(),
+                cargoDocente.getArea().getDivision().getCodigo(),
+                cargoDocente.getTipoCargo().getDescripcion(),
+                cargoDocente.getEstado().getDescripcion()
+        );
+
+        List<IContacto> contactosJefe = cargoDocente
+                .getArea()
+                .getDivision()
+                .getJefe()
+                .getPersona()
+                .getContactos();
+
+        // Se envía un mail a cada contacto del tipo MailLaboral del jefe:
+        StringBuilder destinos = new StringBuilder();
+        for (IContacto contacto : contactosJefe) {
+            if (contacto.getTipo() == "MailLaboral") {
+                destinos.append(contacto.getValor())
+                        .append(",");
+            }
+        }
+        destinos.setLength(destinos.length());
+
+        mail.enviarEmail(destinos.toString(), asunto, mensaje);
+    }
+}
