1c3c3a82f07188f031fbbf83011685e32e85aef1
Tomás <tomasjuran96@gmail.com>
2018-05-10 13:32:38 -0300

    Haciendo funcional el controlador Docentes

--- src/main/java/vista/controladores/Docentes.java
+++ src/main/java/vista/controladores/Docentes.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import auxiliares.Numeros;
 import controlador.ControlDocente;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -29,6 +30,7 @@ import modelo.docente.ICargoDocente;
  * @author Martín Tomás Juran
  * @version 1.0, 1 de may. de 2018
  */
+@SuppressWarnings("rawtypes")
 public class Docentes {
 
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -40,68 +42,82 @@ public class Docentes {
 
 // ----------------------------- Pestaña Cargos ----------------------------- //
 	public class FilaCargo {
-        public Integer idCargo;
-        public String area;
-        public String cargo;
-        public String estado;
-        
-		public FilaCargo(Integer idCargo, String area, String cargo, String estado) {
+		public Integer id;
+		public String area;
+		public String cargo;
+		public String estado;
+		public FilaCargo(Integer id, String area,
+				String cargo, String estado) {
 			super();
-			this.idCargo = idCargo;
+			this.id = id;
 			this.area = area;
 			this.cargo = cargo;
 			this.estado = estado;
 		}
-		public Integer getIdCargo() {
-			return idCargo;
-		}
-		public void setIdCargo(Integer idCargo) {
-			this.idCargo = idCargo;
-		}
-		public String getArea() {
-			return area;
-		}
-		public void setArea(String area) {
-			this.area = area;
-		}
-		public String getCargo() {
-			return cargo;
-		}
-		public void setCargo(String cargo) {
-			this.cargo = cargo;
-		}
-		public String getEstado() {
-			return estado;
-		}
-		public void setEstado(String estado) {
-			this.estado = estado;
-		}
-    }
+		public Integer getId() { return id; }
+		public void setId(Integer idCargo) { this.id = idCargo; }
+		public String getArea() { return area; }
+		public void setArea(String area) { this.area = area; }
+		public String getCargo() { return cargo; }
+		public void setCargo(String cargo) { this.cargo = cargo; }
+		public String getEstado() { return estado; }
+		public void setEstado(String estado) { this.estado = estado; }
+	}
 
-	private ObservableList<FilaCargo> filasCargosDocentes;
+	private ICargoDocente cargoDocenteSeleccionado;
 	private List<ICargoDocente> listaCargosDocentes;
-    private ICargoDocente cargoDocenteSeleccionado;
-    private IArea areaSeleccionada;
-    private ICargo cargoSeleccionado;
+	private ObservableList<FilaCargo> filasCargosDocentes;
 
-    private void vaciarCampos() {
-        txtCargosArea.clear();
-        txtCargosCargo.clear();
-        txtCargosDisp.clear();
-        dtpCargosDispDesde.getEditor().clear();
-        dtpCargosDispHasta.getEditor().clear();
-        txtCargosRes.clear();
-        dtpCargosResDesde.getEditor().clear();
-        dtpCargosResHasta.getEditor().clear();
-        txtCargosCosto.clear();
-        dtpCargosCosto.getEditor().clear();
-    }
+	private void vaciarCamposCargos() {
+		txtCargosArea.clear();
+		txtCargosCargo.clear();
+		txtCargosDisp.clear();
+		dtpCargosDispDesde.getEditor().clear();
+		dtpCargosDispHasta.getEditor().clear();
+		txtCargosRes.clear();
+		dtpCargosResDesde.getEditor().clear();
+		dtpCargosResHasta.getEditor().clear();
+		txtCargosCosto.clear();
+		dtpCargosCosto.getEditor().clear();
+	}
+
+	@SuppressWarnings("unchecked")
+	@FXML public void inicializarTablaCargos() {
+
+		this.colCargosId.setCellValueFactory(
+				new PropertyValueFactory<FilaCargo,Integer>("id"));
+		this.colCargosArea.setCellValueFactory(
+				new PropertyValueFactory<FilaCargo,String>("area"));
+		this.colCargosCargo.setCellValueFactory(
+				new PropertyValueFactory<FilaCargo,String>("cargo"));
+		this.colCargosEstado.setCellValueFactory(
+				new PropertyValueFactory<FilaCargo,String>("estado"));
+
+		this.filasCargosDocentes = FXCollections.observableArrayList();
+		this.tblCargosDocentes.setItems(this.filasCargosDocentes);
+	}
+
+	@FXML private TableView tblCargosDocentes;
+	@FXML private TableColumn colCargosId;
+	@FXML private TableColumn colCargosArea;
+	@FXML private TableColumn colCargosCargo;
+	@FXML private TableColumn colCargosEstado;
+
+	@FXML public void seleccionarCargoDocente() {
+		// TODO cargoDocenteSeleccionado = seleccionado de tblCargoDocente;
+		FilaCargo fila = (FilaCargo) tblCargosDocentes.getSelectionModel().getSelectedItem();
+		ICargoDocente cd = this.control.getICargoDocente();
+		cd.setId(fila.getId());
+		// Recuperar de la BD fila.getId()
+		// cargoDocenteSeleccionado
+	}
 
 	@FXML private Button btnCargosNuevo;
 	@FXML public void nuevoCargo() {
 		// Obtener un ICargoDocente vacío
 		cargoDocenteSeleccionado = this.control.getICargoDocente();
 //		vaciarCampos();
+	
 		/* Prueba */
 		Area a = new Area("B1", "Biología 1", null, null, null, null, null, null);
 		Cargo c = new Cargo(1, "Profesor adjunto semiexclusivo", 40);
@@ -117,20 +133,19 @@ public class Docentes {
 
 	@FXML private Button btnCargosGuardar;
 	@FXML public void guardarCargo() {
+		/*
+		EstadoCargo estado = cmbCargosEstado.getValue();
+		TipoCargo tipoCargo = cmbCargosTipo.getValue();
 	    String disposicion = txtCargosDisp.getText();
         LocalDate dispDesde = dtpCargosDispDesde.getValue();
         LocalDate dispHasta = dtpCargosDispHasta.getValue();
-        float ultimoCosto = Float.parseFloat(txtCargosCosto.getText());
-        LocalDate fechaUltCost = dtpCargosCosto.getValue();
         String resolucion = txtCargosRes.getText();
         LocalDate resDesde = dtpCargosResDesde.getValue();
         LocalDate resHasta = dtpCargosResHasta.getValue();
-        EstadoCargo estado = cmbCargosEstado.getValue();
-        TipoCargo tipoCargo = cmbCargosTipo.getValue();
-
-		cargoDocenteSeleccionado.setArea(areaSeleccionada);
-	    cargoDocenteSeleccionado.setCargo(cargoSeleccionado);
-	    cargoDocenteSeleccionado.setDispDesde(dispDesde);
+        float ultimoCosto = Float.parseFloat(txtCargosCosto.getText());
+        LocalDate fechaUltCost = dtpCargosCosto.getValue();
+        
+		cargoDocenteSeleccionado.setDispDesde(dispDesde);
 	    cargoDocenteSeleccionado.setDispHasta(dispHasta);
 	    cargoDocenteSeleccionado.setDisposicion(disposicion);
 	    cargoDocenteSeleccionado.setEstado(estado);
@@ -140,19 +155,41 @@ public class Docentes {
 	    cargoDocenteSeleccionado.setResHasta(resHasta);
 	    cargoDocenteSeleccionado.setTipoCargo(tipoCargo);
 	    cargoDocenteSeleccionado.setUltimoCosto(ultimoCosto);
-
-	    if (cargoDocenteSeleccionado.getId() == -1) {
-	    	// Se agrega un nuevo Cargo Docente
-	    	//this.control.agregarCargoDocente(docenteSeleccionado, cargoDocenteSeleccionado);
-		} else {
-			// Se modifica un Cargo Docente anterior
-			//this.control.modificarCargoDocente(docenteSeleccionado, cargoDocenteSeleccionado);
+	    */ 
+	    
+		try {
+			cargoDocenteSeleccionado.setUltimoCosto(
+		    		Numeros.stringToFloat(txtCargosCosto.getText()));
+			
+			cargoDocenteSeleccionado.setArea(areaSeleccionada);
+			cargoDocenteSeleccionado.setCargo(cargoSeleccionado);
+			cargoDocenteSeleccionado.setEstado(cmbCargosEstado.getValue());
+			cargoDocenteSeleccionado.setTipoCargo(cmbCargosTipo.getValue());
+			cargoDocenteSeleccionado.setDisposicion(txtCargosDisp.getText());
+			cargoDocenteSeleccionado.setDispDesde(dtpCargosDispDesde.getValue());
+			cargoDocenteSeleccionado.setDispHasta(dtpCargosDispHasta.getValue());
+			cargoDocenteSeleccionado.setResolucion(txtCargosRes.getText());
+			cargoDocenteSeleccionado.setResDesde(dtpCargosResDesde.getValue());
+			cargoDocenteSeleccionado.setResHasta(dtpCargosResHasta.getValue());
+			
+			cargoDocenteSeleccionado.setFechaUltCost(dtpCargosCosto.getValue());
+			
+			if (cargoDocenteSeleccionado.getId() == -1) {
+				// Se agrega un nuevo Cargo Docente
+				//this.control.agregarCargoDocente(docenteSeleccionado, cargoDocenteSeleccionado);
+			} else {
+				// Se modifica un Cargo Docente anterior
+				//this.control.modificarCargoDocente(docenteSeleccionado, cargoDocenteSeleccionado);
+			}
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
 		}
 	}
 
 	@FXML private Button btnCargosDescartar;
 	@FXML public void descartarCargo() {
-	    vaciarCampos();
+	    vaciarCamposCargos();
+	    // actualizarCamposCargos(); Se ejecuta cuando se selecciona un cargo docente
 	}
 
 	@FXML private Button btnCargosEliminar;
@@ -160,35 +197,14 @@ public class Docentes {
 		// TODO Enviar a eliminar el cargoDocenteSeleccionado
 	}
 
-	@FXML public void inicializarTablaCargos() {
-		this.colCargosID.setCellValueFactory(
-				new PropertyValueFactory<FilaCargo,Integer>("idCargo"));
-		this.colCargosArea.setCellValueFactory(
-				new PropertyValueFactory<FilaCargo,String>("area"));
-		this.colCargosCargo.setCellValueFactory(
-				new PropertyValueFactory<FilaCargo,String>("cargo"));
-		this.colCargosEstado.setCellValueFactory(
-				new PropertyValueFactory<FilaCargo,String>("estado"));
-		
-		this.filasCargosDocentes = FXCollections.observableArrayList();
-		this.tblCargosDocentes.setItems(this.filasCargosDocentes);
-	}
+
 	
-	@FXML private TableView<FilaCargo> tblCargosDocentes;
-	@FXML private TableColumn colCargosID;
-	@FXML private TableColumn colCargosArea;
-	@FXML private TableColumn colCargosCargo;
-	@FXML private TableColumn colCargosEstado;
-
-	@FXML public void seleccionarCargoDocente() {
-		// TODO cargoDocenteSeleccionado = seleccionado de tblCargoDocente;
-	    FilaCargo fila = tblCargosDocentes.getSelectionModel().getSelectedItem();
-
-	}
-
+	
+	private IArea areaSeleccionada;
 	@FXML private TextField txtCargosArea;
 	@FXML private Button btnCargosArea;
 
+	private ICargo cargoSeleccionado;
 	@FXML private TextField txtCargosCargo;
 	@FXML private Button btnCargosCargo;
 
