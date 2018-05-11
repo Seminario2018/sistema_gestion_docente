422aae7b77460dd8134c7a672779893f32acc72a
LeonMarchetti <mleonardoa@gmail.com>
2018-05-10 23:33:06 -0300

    Ahora se personaliza el mensaje según la operación realizada

--- src/main/java/mail/NotificacionCargo.java
+++ src/main/java/mail/NotificacionCargo.java
@@ -2,6 +2,7 @@ package mail;
 
 import java.util.List;
 
+import modelo.auxiliares.EstadoOperacion;
 import modelo.docente.ICargoDocente;
 import modelo.docente.IDocente;
 import modelo.persona.IContacto;
@@ -12,10 +13,31 @@ public class NotificacionCargo {
     private static String mailDesde = "semint2018@gmail.com";
     private static String contrasena = "semintunlu";
 
-    public static void notificar(IDocente docente, ICargoDocente cargoDocente) {
-        String asunto = "Actualización de cargo";
+    public static void notificar(EstadoOperacion estadoOperacion, IDocente docente, ICargoDocente cargoDocente) {
+
+        String operacionAsunto;
+        String operacionMensaje;
+
+        switch (estadoOperacion.getEstado()) {
+            case INSERT_OK:
+                operacionAsunto = "Adición";
+                operacionMensaje = "agregó";
+                break;
+            case UPDATE_OK:
+                operacionAsunto = "Actualización";
+                operacionMensaje = "actualizó";
+                break;
+            case DELETE_OK:
+                operacionAsunto = "Quita";
+                operacionMensaje = "quitó";
+                break;
+            default:
+                throw new UnsupportedOperationException("EstadoOperacion no soportado");
+        }
+
+        String asunto = String.format("%s de cargo", operacionAsunto);
         String mensaje = String.format(
-                "Se actualizó el siguiente cargo para el docente:\n"
+                "Se %s el siguiente cargo para el docente:\n"
                 + "\tLegajo: %d\n"
                 + "\tApellido: %s\n"
                 + "\tNombre: %s\n"
@@ -25,6 +47,7 @@ public class NotificacionCargo {
                 + "\tDivisión: %d\n"
                 + "\tTipo de cargo: %s\n"
                 + "\tEstado de cargo: %s\n",
+                operacionMensaje,
                 docente.getLegajo(),
                 docente.getPersona().getApellido(),
                 docente.getPersona().getNombre(),
