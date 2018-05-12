dd9510fc675cfc4f5c809748773f4a4555d7162b
JackTheBlack <ascona.nicolas@gmail.com>
2018-05-12 08:04:58 -0300

    cambios gest area y division

--- src/main/java/modelo/division/GestorArea.java
+++ src/main/java/modelo/division/GestorArea.java
@@ -3,25 +3,51 @@ package modelo.division;
 import java.util.List;
 
 import modelo.auxiliares.EstadoOperacion;
+import modelo.auxiliares.EstadoOperacion.CodigoEstado;
+import persistencia.ManejoDatos;
 
 public class GestorArea {
 
     public EstadoOperacion nuevaArea(IArea area) {
         // TODO actualizar BD
-        return new EstadoOperacion(EstadoOperacion.CodigoEstado.INSERT_OK,
-                "El área se creó correctamente.");
+          try {
+              ManejoDatos e = new ManejoDatos();
+              String table = "areas";
+              String campos = "`Codigo`, `Descripcion`, `Division`,`Responsable`, `Disposicion`,`Desde`,`Hasta`,`SubAreaDe`";
+              String valores = "\'" + area.getCodigo() + "\', \'" +area.getDescripcion() + "\', \'" + area.getDivision() +"\',\'"+area.getDocenteResponsable().getLegajo()+"\',\' "+area.getDispDesde() +"\',\' "+area.getDispHasta() +"\',\' "+area.getAreaDe().getCodigo() +"`";
+              e.insertar(table, campos, valores);
+              return e.isEstado()?new EstadoOperacion(CodigoEstado.INSERT_OK, "El Area se creo correctamente"):new EstadoOperacion(CodigoEstado.INSERT_ERROR, "No se pudo crear el Proyecto");
+          } catch (Exception var6) {
+              return new EstadoOperacion(CodigoEstado.INSERT_ERROR, "No se pudo crear el Area");
+          }
     }
 
     public EstadoOperacion modificarArea(IArea area) {
         // TODO actualizar BD
-        return new EstadoOperacion(EstadoOperacion.CodigoEstado.UPDATE_OK,
-                "El área se modificó correctamente");
+          try {
+              ManejoDatos e = new ManejoDatos();
+              String tabla = "Areas";
+              String campos = "`Codigo` = \'" + area.getCodigo() + "\', `Descripcion` = \'" + area.getDescripcion() + "\', `Division`= \'" + area.getDivision().getCodigo() +"\', `Responsable`= \'" + area.getDocenteResponsable().getLegajo()+"\'`Desde`"+area.getDispDesde()+"\',`Hasta`"+area.getDispHasta()+"\',`SubArea_De`=\'"+area.getAreaDe().getCodigo()+"'";
+              String condicion = "`Codigo` = \'" + area.getCodigo() + "\'";
+              e.update(tabla, campos, condicion);
+              return new EstadoOperacion(CodigoEstado.UPDATE_OK, "El Area se modificÃƒÂ³ correctamente");
+          } catch (Exception var6) {
+              return new EstadoOperacion(CodigoEstado.UPDATE_ERROR, "No se pudo modificar el Area");
+          }
     }
 
     public EstadoOperacion eliminarArea(IArea area) {
         // TODO actualizar BD
-        return new EstadoOperacion(EstadoOperacion.CodigoEstado.DELETE_OK,
-                "El área se eliminó correctamente");
+         try {
+             ManejoDatos e = new ManejoDatos();
+           
+             e.delete("`areas`", "Codigo = " + area.getCodigo());
+             return new EstadoOperacion(CodigoEstado.DELETE_OK, "El area se eliminÃƒÂ³ correctamente");
+         } catch (Exception var3) {
+             return new EstadoOperacion(CodigoEstado.DELETE_ERROR, "No se pudo eliminar el area");
+         }
+        
+        
     }
 
     public List<IArea> listarArea(IArea area) {
--- src/main/java/modelo/division/GestorDivision.java
+++ src/main/java/modelo/division/GestorDivision.java
@@ -19,37 +19,37 @@ import persistencia.ManejoDatos;
 
 public class GestorDivision {
 
-    private String armarCondicion(IDivision division) {
+  private String armarCondicion(IDivision division) {
 
-        String condicion = "TRUE";
-        if (division != null) {
-            condicion = "";
-            //division
-            if (division.getCodigo() != 0) {
-                condicion += " `Codigo` = " + division.getCodigo();
-            }
-            if (division.getDescripcion() != null) {
-                condicion += " `Descripcion` = '" + division.getDescripcion() + "'";
-            }
-            if (division.getJefe().getLegajo() !=0 ) {
-                condicion += " `Jefe` = '" + division.getJefe() + "'";
-            }
-            if (division.getDisposicion() != null) {
-                condicion += " `Disposicion` = '" + division.getDisposicion() + "'";
-            }
-            if (division.getDispDesde()!= null) {
-                condicion += " `Desde` = '" + Date.valueOf(division.getDispDesde()).toString() + "'";
-            }
-            if (division.getDispHasta() != null) {
-                condicion += " `Hasta` = " +  Date.valueOf(division.getDispHasta()).toString() + "'";
-            }
-        }
-        return condicion; 
+    String condicion = "TRUE";
+    if (division != null) {
+      condicion = "";
+      //division
+      if (division.getCodigo() != 0) {
+        condicion += " `Codigo` = " + division.getCodigo();
+      }
+      if (division.getDescripcion() != null) {
+        condicion += " `Descripcion` = '" + division.getDescripcion() + "'";
+      }
+      if (division.getJefe().getLegajo() !=0 ) {
+        condicion += " `Jefe` = '" + division.getJefe() + "'";
+      }
+      if (division.getDisposicion() != null) {
+        condicion += " `Disposicion` = '" + division.getDisposicion() + "'";
+      }
+      if (division.getDispDesde()!= null) {
+        condicion += " `Desde` = '" + Date.valueOf(division.getDispDesde()).toString() + "'";
+      }
+      if (division.getDispHasta() != null) {
+        condicion += " `Hasta` = " +  Date.valueOf(division.getDispHasta()).toString() + "'";
+      }
     }
-    
-    public EstadoOperacion nuevaDivision(IDivision division) {
+    return condicion; 
+  }
+  
+  public EstadoOperacion nuevaDivision(IDivision division) {
         // TODO actualizar BD
-           try {
+         try {
                ManejoDatos e = new ManejoDatos();
                String table = "divisiones";
                String campos = "`Codigo`, `Descripcion`, `Jefe`, `Disposicion`, `Desde`, `Hasta`";
@@ -63,7 +63,7 @@ public class GestorDivision {
 
     public EstadoOperacion modificarDivision(IDivision division) {
         // TODO actualizar BD
-          try {
+        try {
               ManejoDatos e = new ManejoDatos();
               String tabla = "divisiones";
               String campos = "`Codigo` = \'" + division.getCodigo() + "\', `Descripcion` = \'" + division.getDescripcion() + "\', `Jefe`= \'" + division.getJefe().getLegajo() + "\', `Disposicion`= \'" + division.getDisposicion() + "\', `Desde` = \'" + division.getDispDesde() + "\', `Hasta` =\'" + division.getDispHasta() + "'";
@@ -77,9 +77,10 @@ public class GestorDivision {
 
     public EstadoOperacion eliminarDivision(IDivision division) {
         // TODO actualizar BD
-         try {
+      
+      try {
              ManejoDatos e = new ManejoDatos();
-           
+             e.delete("`Area`", "Responsable = " +division.getCodigo()  );
              e.delete("`Division`", "Codigo = " + division.getCodigo());
              return new EstadoOperacion(CodigoEstado.DELETE_OK, "La division se eliminÃƒÂ³ correctamente");
          } catch (Exception var3) {
@@ -88,43 +89,43 @@ public class GestorDivision {
     }
 
     public List<IDivision> listarDivision(IDivision division) {
-        ArrayList<Hashtable<String, String>> res = new ArrayList<Hashtable<String, String>>();
+      ArrayList<Hashtable<String, String>> res = new ArrayList<Hashtable<String, String>>();
         ArrayList<IDivision> divisiones = new ArrayList<IDivision>();
-        
+      
         String tabla = "Division";
         String campos = "*";
         String condicion = this.armarCondicion(division);
+      
+      
+      
+      try {
+          ManejoDatos md = new ManejoDatos(); 
+          res = md.select(tabla, campos, condicion);
+          for (Hashtable<String, String> reg : res) {
+        Division div = new Division(
+              reg.get("Usuario").toString(),
+              new HashSalt(reg.get("Hash").toString(), reg.get("Salt").toString()),
+              reg.get("Descripcion").toString(),
+              new ArrayList<IRol>()
+            );
         
+        GestorPersona gp = new GestorPersona();
+        Persona p = new Persona();
+        p.setTipoDocumento(TipoDocumento.getTipo(new TipoDocumento(Integer.parseInt(reg.get("TipoDocumento")), null)));
+        p.setNroDocumento(Integer.parseInt(reg.get("NroDocumento")));
+        p = (Persona) gp.listarPersonas(p).get(0);
         
+        divisiones.add(div);        
         
-        try {
-            ManejoDatos md = new ManejoDatos(); 
-            res = md.select(tabla, campos, condicion);
-            for (Hashtable<String, String> reg : res) {
-                Division div = new Division(
-                            reg.get("Usuario").toString(),
-                            new HashSalt(reg.get("Hash").toString(), reg.get("Salt").toString()),
-                            reg.get("Descripcion").toString(),
-                            new ArrayList<IRol>()
-                        );
-                
-                GestorPersona gp = new GestorPersona();
-                Persona p = new Persona();
-                p.setTipoDocumento(TipoDocumento.getTipo(new TipoDocumento(Integer.parseInt(reg.get("TipoDocumento")), null)));
-                p.setNroDocumento(Integer.parseInt(reg.get("NroDocumento")));
-                p = (Persona) gp.listarPersonas(p).get(0);
-                
-                divisiones.add(div);                
-                
-            }
-            return divisiones;
+      }
+          return divisiones;
         }catch(Exception e) {
-            return divisiones;
+          return divisiones;
         }
         
     }
     
     
-    
-    
+  
+  
 }
