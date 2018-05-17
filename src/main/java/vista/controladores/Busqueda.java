db2e8247fa7575bffb13b2d21c66289a35885032
Tomás <tomasjuran96@gmail.com>
2018-05-17 19:16:26 -0300

    Busqueda y BusquedaCargoDocente esqueleto

--- src/main/java/vista/controladores/Busqueda.java
+++ src/main/java/vista/controladores/Busqueda.java
@@ -4,6 +4,11 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 /**
  * @author Martín Tomás Juran
@@ -16,6 +21,20 @@ public class Busqueda implements Initializable {
 	 */
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+	}
+	
+	@FXML private TableView<?> tblBusqueda;
+	
+	@FXML private TextField txtBusquedaCriterio;
+	
+	@FXML private Button btnBusquedaNuevo;
+	@FXML public void nuevo(ActionEvent event) {
+		
+	}
+	
+	@FXML private Button btnBusquedaSeleccionar;
+	@FXML public void seleccionar(ActionEvent event) {
+		
 	}
 
 }
