6c77f946794ff99be31bf69c254b657889ebac9f
Tomás <tomasjuran96@gmail.com>
2018-05-17 19:37:10 -0300

    Esqueleto Usuarios

--- src/main/java/vista/controladores/Usuarios.java
+++ src/main/java/vista/controladores/Usuarios.java
@@ -4,6 +4,13 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 /**
  * @author Martín Tomás Juran
@@ -17,5 +24,66 @@ public class Usuarios implements Initializable {
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 	}
+	
+// -------------------------------- General --------------------------------- //
 
+	@FXML private TextField txtDocumento;
+	@FXML private TextField txtNombre;
+
+	@FXML private Button btnSeleccionarPersona;
+	@FXML public void seleccionarPersona(ActionEvent event) {
+		
+	}
+	
+	@FXML private TextField txtUsuario;
+	@FXML private PasswordField txtContrasena;
+	@FXML private PasswordField txtConfirmar;
+	@FXML private TextField txtDescripcion;
+	
+	@FXML private Button btnBuscar;
+	@FXML public void buscarUsuario(ActionEvent event) {
+		
+	}
+	
+	@FXML private Button btnNuevo;
+	@FXML public void nuevoUsuario(ActionEvent event) {
+		
+	}
+	
+	@FXML private Button btnGuardar;
+	@FXML public void guardarUsuario(ActionEvent event) {
+		
+	}
+	
+	@FXML private Button btnDescartar;
+	@FXML public void descartarUsuario(ActionEvent event) {
+		
+	}
+	
+	@FXML private Button btnEliminar;
+	@FXML public void eliminarUsuario(ActionEvent event) {
+		
+	}
+
+	
+// --------------------------------- Roles ---------------------------------- //
+	
+	@FXML private Button btnAgregar;
+	@FXML public void agregarRol(ActionEvent event) {
+		
+	}
+	
+	@FXML private Button btnQuitar;
+	@FXML public void quitarRol(ActionEvent event) {
+		
+	}
+	
+	@FXML private TableView<?> tblDisponibles;
+	@FXML private TableColumn<?, ?> colDisponibles; // Descripción del rol
+	
+	@FXML private TableView<?> tblUsuario;
+	@FXML private TableColumn<?, ?> colUsuario; // Descripción del rol
+	
 }
+
+
