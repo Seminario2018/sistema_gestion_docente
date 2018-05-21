badb7ff38f96ffeca9ce9b403f596b2f35ac947e
leomarazzo <leonardojmarazzo@gmail.com>
2018-05-21 20:01:51 -0300

    Guardado y recuperacion de axiliares

--- src/main/java/modelo/auxiliares/EstadoDocente.java
+++ src/main/java/modelo/auxiliares/EstadoDocente.java
@@ -29,12 +29,89 @@ public class EstadoDocente {
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
+    
+    public void guardar() {
+    	if(this.existe()) {
+    		this.actualizar();
+    	}else {
+    		this.insertar();
+    	}
+    }
+
+	private void insertar() {
+		this.setId(EstadoDocente.getMaxID()+1);
+		try {
+			ManejoDatos md = new ManejoDatos();
+			String tabla = "EstadosDocentes";
+			String campos = "id, Descripcion";			
+			String valores = this.getId() + ", '" + this.getDescripcion() + "'";
+			
+			md.insertar(tabla, campos, valores);
+			
+		}catch (Exception e){
+			
+		}
+		
+	}
+
+	private void actualizar() {
+		try {
+			ManejoDatos md = new ManejoDatos();
+			String tabla = "EstadosDocentes";
+			String campos = "Descripcion = '" + this.getDescripcion() + "'";
+			String condicion = "id = " + this.getId();
+			
+			md.update(tabla, campos, condicion);
+			
+		}catch (Exception e){
+			
+		}
+		
+	}
+
+	private boolean existe() {
+		try {
+			ManejoDatos md = new ManejoDatos();
+			String tabla = "EstadosDocentes";
+			String campos = "id";
+			String condicion = "id = " + this.getId();
+			
+			if (md.select(tabla, campos, condicion).isEmpty()) {
+				return true;
+			}else {
+				return false;
+			}
+			
+			
+		}catch (Exception e){
+			return false;
+		}
+		
+	}
+    
 
 	/**
 	 * @return la lista de estados del docente de la BD
 	 */
 	public static List<EstadoDocente> getLista() {
-		return null;
+		try {
+			List<EstadoDocente> estados = new ArrayList<EstadoDocente>(); 
+			ManejoDatos md = new ManejoDatos();
+			String tabla = "EstadosDocentes";
+			String campos = "*";
+			
+			ArrayList<Hashtable<String, String>> res = md.select(tabla, campos);
+			for (Hashtable<String, String> reg : res) {
+				EstadoDocente estado = new EstadoDocente();
+				estado.setId(Integer.parseInt(reg.get("id")));
+				estado.setDescripcion(reg.get("Descripcion"));
+				estados.add(estado);
+			}
+
+			return estados;
+		} catch (NumberFormatException e) {
+			return new ArrayList<EstadoDocente>();
+		}
 	}
 
 	public static EstadoDocente getEstado(EstadoDocente estado) {
@@ -53,6 +130,24 @@ public class EstadoDocente {
 		} catch (NumberFormatException e) {
 			return new EstadoDocente();
 		}
+	}
+	
+	
+	public static int getMaxID() {
+		try {
+			ManejoDatos md = new ManejoDatos();
+			String tabla = "EstadosDocentes";
+			String campos = "Max(id)";
+			
+			ArrayList<Hashtable<String, String>> res = md.select(tabla, campos);
+			int maxID = Integer.parseInt(res.get(0).get("Max(id)"));
+			return maxID;
+			
+			
+		}catch (Exception e){
+			return 0;
+		}
+
 	}
 
 	@Override
--- src/main/java/modelo/auxiliares/EstadoProyecto.java
+++ src/main/java/modelo/auxiliares/EstadoProyecto.java
@@ -1,6 +1,10 @@
 package modelo.auxiliares;
 
+import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
+
+import persistencia.ManejoDatos;
 
 /**
  * @author Martín Tomás Juran
@@ -26,14 +30,108 @@ public class EstadoProyecto {
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
+    
+    
+    public void guardar() {
+    	if(this.existe()) {
+    		this.actualizar();
+    	}else {
+    		this.insertar();
+    	}
+    }
+
+	private void insertar() {
+		this.setId(EstadoProyecto.getMaxID() + 1);
+		try {
+			ManejoDatos md = new ManejoDatos();
+			String tabla = "EstadosProyectos";
+			String campos = "idEstadoProyecto, Descripcion";			
+			String valores = this.getId() + ", '" + this.getDescripcion() + "'";
+			
+			md.insertar(tabla, campos, valores);
+			
+		}catch (Exception e){
+			
+		}
+		
+	}
+
+
+	private void actualizar() {
+		try {
+			ManejoDatos md = new ManejoDatos();
+			String tabla = "EstadosProyectos";
+			String campos = "Descripcion = '" + this.getDescripcion() + "'";
+			String condicion = "idEstadoProyecto = " + this.getId();
+			
+			md.update(tabla, campos, condicion);
+			
+		}catch (Exception e){
+			
+		}
+		
+	}
+
+	private boolean existe() {
+		try {
+			ManejoDatos md = new ManejoDatos();
+			String tabla = "EstadosProyectos";
+			String campos = "idEstadoProyecto";
+			String condicion = "idEstadoProyecto = " + this.getId();
+			
+			if (md.select(tabla, campos, condicion).isEmpty()) {
+				return true;
+			}else {
+				return false;
+			}
+			
+			
+		}catch (Exception e){
+			return false;
+		}
+		
+	}
 
     /**
      * @return la lista de estados del docente de la BD
      */
     public static List<EstadoProyecto> getLista() {
-        return null;
+        try {
+        	List<EstadoProyecto> estados = new ArrayList<EstadoProyecto>();
+        	ManejoDatos md = new ManejoDatos();
+        	String tabla = "EstadosProyectos";
+        	String campos = "*";
+        	
+        	ArrayList<Hashtable<String, String>> res = md.select(tabla, campos);
+        	for (Hashtable<String, String> reg : res) {
+				EstadoProyecto estado = new EstadoProyecto();
+				estado.setId(Integer.parseInt(reg.get("idEstadoProyecto")));
+				estado.setDescripcion(reg.get("Descripcion"));
+				estados.add(estado);
+			}
+        	
+        	return estados;        	
+        }catch (Exception e) {
+        	return new ArrayList<EstadoProyecto>();
+        }
     }
 
+    
+    public static int getMaxID() {
+		try {
+			ManejoDatos md = new ManejoDatos();
+			String tabla = "EstadosProyectos";
+			String campos = "Max(idEstadoProyecto)";
+			
+			ArrayList<Hashtable<String, String>> res = md.select(tabla, campos);
+			int maxID = Integer.parseInt(res.get(0).get("Max(idEstadoProyecto)"));
+			
+			return maxID;
+		}catch (Exception e) {
+			return 0;
+		}
+	}
+    
     @Override
     public String toString() {
         return this.descripcion;
