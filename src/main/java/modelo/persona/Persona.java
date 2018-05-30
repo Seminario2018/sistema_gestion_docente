91e5c12f703d92f57b1710cddcd5baaeea3debbf
LeonMarchetti <mleonardoa@gmail.com>
2018-05-30 14:08:27 -0300

    Método para sacar el nombre completo (apellido + nombre)

--- src/main/java/modelo/persona/Persona.java
+++ src/main/java/modelo/persona/Persona.java
@@ -2,7 +2,6 @@ package modelo.persona;
 
 import java.time.LocalDate;
 import java.util.List;
-
 import modelo.auxiliares.EstadoPersona;
 import modelo.auxiliares.TipoDocumento;;
 
@@ -19,9 +18,9 @@ public class Persona implements IPersona {
 	private int nroDocumento;
 	private List<IDomicilio> domicilios;
 	private List<IContacto> contactos;
-	private List<ITitulo> titulos;	
+	private List<ITitulo> titulos;
 	private EstadoPersona estado;
-	
+
 	public Persona(String apellido, String nombre, LocalDate fechaNacimiento, TipoDocumento tipoDocumento,
 			int nroDocumento, List<IDomicilio> domicilios, List<IContacto> contactos, List<ITitulo> titulos,
 			EstadoPersona estado) {
@@ -36,7 +35,7 @@ public class Persona implements IPersona {
 		this.titulos = titulos;
 		this.estado = estado;
 	}
-	
+
 	public Persona() {
 		super();
 		this.apellido = null;
@@ -54,27 +53,32 @@ public class Persona implements IPersona {
 	public String getApellido() {
 		return apellido;
 	}
-	
+
 	@Override
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
-	
+
 	@Override
 	public String getNombre() {
 		return nombre;
 	}
-	
+
 	@Override
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
+
+	@Override
+    public String getNombreCompleto() {
+	    return this.apellido + " " + this.nombre;
+	}
+
 	@Override
 	public LocalDate getFechaNacimiento() {
 		return fechaNacimiento;
 	}
-	
+
 	@Override
 	public void setFechaNacimiento(LocalDate fechaNacimiento) {
 		this.fechaNacimiento = fechaNacimiento;
@@ -83,70 +87,70 @@ public class Persona implements IPersona {
 	public TipoDocumento getTipoDocumento() {
 		return tipoDocumento;
 	}
-	
-	
+
+
 	@Override
 	public void setTipoDocumento(TipoDocumento tipoDocumento) {
 		this.tipoDocumento = tipoDocumento;
 	}
-	
-	
+
+
 	@Override
 	public int getNroDocumento() {
 		return nroDocumento;
 	}
-	
+
 	@Override
 	public void setNroDocumento(int nroDocumento) {
 		this.nroDocumento = nroDocumento;
 	}
-	
-	
+
+
 	@Override
 	public List<IDomicilio> getDomicilios() {
 		return domicilios;
 	}
-	
+
 	@Override
 	public void setDomicilios(List<IDomicilio> domicilios) {
 		this.domicilios = domicilios;
 	}
-	
-	
+
+
 	@Override
 	public List<IContacto> getContactos() {
 		return contactos;
 	}
-	
+
 	@Override
 	public void setContactos(List<IContacto> contactos) {
 		this.contactos = contactos;
 	}
-	
+
 	@Override
 	public List<ITitulo> getTitulos() {
 		return titulos;
 	}
-	
+
 	@Override
 	public void setTitulos(List<ITitulo> titulos) {
 		this.titulos = titulos;
 	}
-	
+
 	@Override
 	public EstadoPersona getEstado() {
 		return estado;
 	}
-	
+
 	@Override
 	public void setEstado(EstadoPersona estado) {
 		this.estado = estado;
 	}
-	
-	
+
+
 	@Override
 	public IPersona clone(){
 		return new Persona(apellido, nombre, fechaNacimiento, tipoDocumento, nroDocumento, domicilios, contactos, titulos, estado);
 	}
-	
+
 }
