f8116270caee01993cf68fb92e905761058174f0
Tomás <tomasjuran96@gmail.com>
2018-05-31 16:51:02 -0300

    Filtros y cálculos

---
+++ src/main/java/modelo/auxiliares/Calculo.java
@@ -0,0 +1,31 @@
+package modelo.auxiliares;
+
+/**
+ * @author Martín Tomás Juran
+ * @version 1.0, 31 de may. de 2018
+ */
+public enum Calculo {
+	SUM ("Suma", "SUM"),
+	COUNT ("Contar", "COUNT"),
+	MAX ("Máximo", "MAX"),
+	MIN ("Mínimo", "MIN");
+	
+	private String descripcion;
+	private String calculo;
+	
+	Calculo(String descripcion, String calculo) {
+		this.descripcion = descripcion;
+		this.calculo = calculo;
+	}	
+	
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public String getCalculo() {
+		return calculo;
+	}
+	
+	public Calculo[] getLista() {
+		return Calculo.values();
+	}
+}
---
+++ src/main/java/modelo/auxiliares/Filtro.java
@@ -0,0 +1,36 @@
+package modelo.auxiliares;
+
+/**
+ * @author Martín Tomás Juran
+ * @version 1.0, 31 de may. de 2018
+ */
+public enum Filtro {
+	IGUAL ("Igual", "="),
+	DISTI ("Distinto", "<>"),
+	MENOR ("Menor", "<"),
+	MENIG ("Menor o igual", "<="),
+	MAYOR ("Mayor", ">"),
+	MAYIG ("Mayor o igual", ">="),
+	CONTI ("Contiene", "LIKE"),
+	EMPIE ("Empieza con", "LIKE"),
+	TERMI ("Termina con", "LIKE");
+	
+	private String descripcion;
+	private String filtro;
+	
+	private Filtro(String descripcion, String filtro) {
+		this.descripcion = descripcion;
+		this.filtro = filtro;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public String getFiltro() {
+		return filtro;
+	}
+
+	public Filtro[] getLista() {
+		return Filtro.values();
+	}
+}
