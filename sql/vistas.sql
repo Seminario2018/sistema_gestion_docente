237ddc29611652b517bdfb1b624fe43bc0596fac
leomarazzo <leonardojmarazzo@gmail.com>
2018-05-31 14:31:47 -0300

    Correcciones vistas

--- sql/vistas.sql
+++ sql/vistas.sql
@@ -10,7 +10,7 @@ CREATE VIEW `ViewCargoDocente` AS
         JOIN (`Docentes` `d`
         JOIN `Personas` `p` ON (((`d`.`TipoDocumento` = `p`.`TipoDocumento`)
             AND (`d`.`NroDocumento` = `p`.`NroDocumento`)))) ON ((`cd`.`Legajo` = `d`.`Legajo`)))
-        JOIN `Cargos` `c` ON ((`cd`.`Cargo` = `c`.`Codigo`)))
+        JOIN `Cargos` `c` ON ((`cd`.`Cargo` = `c`.`Codigo`)));
 
 CREATE VIEW `ViewPersona` AS
     SELECT 
@@ -18,7 +18,7 @@ CREATE VIEW `ViewPersona` AS
         `p`.`Nombre` AS `Nombre`,
         `p`.`Apellido` AS `Apellido`
     FROM
-        (`Personas` `p`)
+        (`Personas` `p`);
 
 
 CREATE VIEW `ViewDocente` AS
@@ -29,29 +29,25 @@ CREATE VIEW `ViewDocente` AS
     FROM
         (`Docentes`
         JOIN `Personas` ON (((`Docentes`.`TipoDocumento` = `Personas`.`TipoDocumento`)
-            AND (`Docentes`.`NroDocumento` = `Personas`.`NroDocumento`))))
-
-
-CREATE VIEW `ViewDocente` AS
-    SELECT 
-        `Docentes`.`Legajo` AS `Legajo`,
-        `Personas`.`Nombre` AS `Nombre`,
-        `Personas`.`Apellido` AS `Apellido`
-    FROM
-        (`Docentes`
-        JOIN `Personas` ON (((`Docentes`.`TipoDocumento` = `Personas`.`TipoDocumento`)
-            AND (`Docentes`.`NroDocumento` = `Personas`.`NroDocumento`))))
+            AND (`Docentes`.`NroDocumento` = `Personas`.`NroDocumento`))));
 
 
 CREATE VIEW `ViewProyecto` AS
     SELECT 
         `Proyectos`.`id` AS `id`, `Proyectos`.`Nombre` AS `Nombre`
     FROM
-        `Proyectos`
+        `Proyectos`;
 
 CREATE VIEW `ViewUsuario` AS
     SELECT 
         `Usuarios`.`Usuario` AS `Usuario`
     FROM
-        `Usuarios`
+        `Usuarios`;
+        
+CREATE VIEW `ViewRoles` AS
+    SELECT 
+        `Roles`.`id` AS `id`, `Roles`.`nombre` AS `nombre`
+    FROM
+        `Roles`
+
 
