a318611e42e02e53700990093e60da6e771ea1d5
LeonMarchetti <mleonardoa@gmail.com>
2018-06-09 14:32:05 -0300

    Trabajando en manejo de errores en Costeo

--- src/main/java/excel/Costeo.java
+++ src/main/java/excel/Costeo.java
@@ -2,16 +2,18 @@ package excel;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.NumberFormat;
-import java.text.ParseException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import controlador.ControlCargo;
 import controlador.ControlDocente;
 import modelo.auxiliares.EstadoOperacion;
+import modelo.cargo.ICargo;
 import modelo.docente.ICargoDocente;
 import modelo.docente.IDocente;
+import modelo.persona.IPersona;
 
 /**
  * Clase que importa el costeo de una planilla Excel.
@@ -19,49 +21,129 @@ import modelo.docente.IDocente;
  */
 public class Costeo {
 
+    private static ControlCargo ctrlCargo = new ControlCargo();
     private static ControlDocente ctrlDocente = new ControlDocente(null);
 
-    /** Trae de la base de datos el cargoDocente correspondiente.
-     * @param id Id del cargo docente a buscar.
-     * @return El cargoDocente buscado.
-     * @throws Exception Si el cargoDocente no está en la base de datos.
-     * */
-    private static ICargoDocente buscarCargoDocente(int id) throws Exception {
-        ICargoDocente cargoDocenteBusqueda = ctrlDocente.getICargoDocente();
-        cargoDocenteBusqueda.setId(id);
-
-        List<ICargoDocente> listaCargosDocentes = ctrlDocente.listarCargosDocente(null, cargoDocenteBusqueda);
-        if (listaCargosDocentes.isEmpty()) {
-            throw new Exception("CargoDocente no encontrado.");
-        } else {
-            return listaCargosDocentes.get(0);
-        }
-    }
-
     /**
-     * Actualiza el último costo del cargoDocente, y lo persiste en la base de datos.
-     * @param cargoDocente CargoDocente a actualizar.
-     * @param costo Nuevo costo del cargodocente.
-     * @param legajoDocente Legajo del docente.
-     * */
-    private static void actualizarCosto(ICargoDocente cargoDocente, float costo, int legajoDocente) {
-        // Actualizar último costo:
-        cargoDocente.setUltimoCosto(costo);
-        cargoDocente.setFechaUltCost(LocalDate.now());
+     * Clase que analiza cada fila de la planilla de costos
+     * @author LeoAM
+     *
+     */
+    public static class FilaCosteo {
 
-        // Instancia de docente:
-        IDocente d = ctrlDocente.getIDocente();
-        d.setLegajo(legajoDocente);
+        private List<String> errores = new ArrayList<String>();
 
-        // Persistir cambio:
-        EstadoOperacion resultado = ctrlDocente.guardarCargoDocente(d, cargoDocente);
-        switch (resultado.getEstado()) {
-            case UPDATE_ERROR:
-                throw new RuntimeException("Error de modificación de cargo docente: " + resultado.getMensaje());
-            case UPDATE_OK:
-                break;
-            default:
-                throw new RuntimeException("Estado de modificación no esperado: " + resultado.getMensaje());
+        private String strlegajoDocente;
+        private String apellido;
+        private String nombre;
+        private String strCodigoCargo;
+        private String strIdCargoDocente;
+        private String strRemConAporte;
+
+        private float remConAporte;
+        private ICargoDocente cargoDocente;
+        private IDocente docente;
+
+        public FilaCosteo(List<String> fila) {
+            this.strlegajoDocente = fila.get(0).trim();
+            this.apellido = fila.get(1).trim();
+            this.nombre = fila.get(2).trim();
+            this.strCodigoCargo = fila.get(3).trim();
+            this.strIdCargoDocente = fila.get(4).trim();
+            this.strRemConAporte = fila.get(14).trim();
+
+            comprobarDocente();
+            comprobarCargoDocente();
+            comprobarRemuneracion();
+        }
+
+        private void comprobarCargoDocente() {
+            try {
+                int idCargoDocente = Integer.parseInt(this.strIdCargoDocente);
+
+                ICargoDocente cargoDocenteBusqueda = ctrlDocente.getICargoDocente();
+                cargoDocenteBusqueda.setId(idCargoDocente);
+                List<ICargoDocente> listaCargosDocentes = ctrlDocente.listarCargosDocente(null, cargoDocenteBusqueda);
+                if (listaCargosDocentes.isEmpty()) {
+                    errores.add("El cargo no existe");
+                } else {
+                    this.cargoDocente = listaCargosDocentes.get(0);
+                    this.comprobarCargo(cargoDocente.getCargo());
+                }
+
+            } catch (NumberFormatException e) {
+                errores.add("El cargo no es numérico.");
+            }
+        }
+
+        private void comprobarCargo(ICargo cargo) {
+            try {
+                int codigoCargo = Integer.parseInt(this.strCodigoCargo);
+
+                ICargo cargoBusqueda = ctrlCargo.getICargo();
+                cargoBusqueda.setCodigo(codigoCargo);
+                List<ICargo> listaCargos = ctrlCargo.listarCargo(cargoBusqueda);
+                if (listaCargos.isEmpty()) {
+                    // No existe ningún cargo con ese código
+                    errores.add("No existe esa categoría de cargo");
+                } else {
+                    if (cargo.getCodigo() != listaCargos.get(0).getCodigo()) {
+                        errores.add("No coincide la categoría del cargo docente");
+                    }
+                }
+
+            } catch (NumberFormatException e) {
+                errores.add("La categoría no es numérica.");
+            }
+        }
+
+        private void comprobarDocente() {
+            try {
+                int legajoDocente = Integer.parseInt(this.strlegajoDocente);
+                IDocente docenteBusqueda = ctrlDocente.getIDocente();
+                docenteBusqueda.setLegajo(legajoDocente);
+                List<IDocente> listaDocentes = ctrlDocente.listarDocente(docenteBusqueda);
+                if (listaDocentes.isEmpty()) {
+                    errores.add("El docente con ese legajo no existe");
+                } else {
+                    this.docente = listaDocentes.get(0);
+                    this.comprobarPersona();
+                }
+            } catch (NumberFormatException e) {
+                errores.add("El legajo no es numérico");
+            }
+        }
+
+        private void comprobarPersona() {
+            IPersona persona = this.docente.getPersona();
+            if (persona == null) {
+                errores.add("El docente no tiene datos personales asociados");
+            } else {
+                if (!persona.getApellido().equals(this.apellido)) {
+                    errores.add("El apellido no coincide");
+                }
+                if (!persona.getNombre().equals(this.nombre)) {
+                    errores.add("El nombre no coincide");
+                }
+            }
+        }
+
+        private void comprobarRemuneracion() {
+            try {
+                this.remConAporte = Float.parseFloat(this.strRemConAporte);
+            } catch (NumberFormatException e) {
+                errores.add("La remuneración con aporte debe ser numérico");
+            }
+        }
+
+        public String getErrores() {
+            return String.join("\n", this.errores);
+        }
+
+        public EstadoOperacion actualizarCosto() {
+            this.cargoDocente.setUltimoCosto(this.remConAporte);
+            this.cargoDocente.setFechaUltCost(LocalDate.now());
+            return ctrlDocente.guardarCargoDocente(this.docente, this.cargoDocente);
         }
     }
 
@@ -82,21 +164,17 @@ public class Costeo {
         grilla.remove(grilla.size() - 1);
         grilla.remove(grilla.size() - 1);
 
+        int fila = 3;
         for (List<String> filaGrilla : grilla) {
-            try {
-                int legajo = Integer.parseInt(filaGrilla.get(0).trim());
+            FilaCosteo filaCosteo = new FilaCosteo(filaGrilla);
 
-                int id = Integer.parseInt(filaGrilla.get(4).trim());
-                ICargoDocente cargoDocente = buscarCargoDocente(id);
-
-                float remConAporte = NumberFormat.getInstance().parse(filaGrilla.get(14)).floatValue();
-                actualizarCosto(cargoDocente, remConAporte, legajo);
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-            } catch (ParseException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
+            // TODO Mostrar en vista
+            System.out.printf("Fila: %s\n", fila++);
+            String errores = filaCosteo.getErrores();
+            if (errores.equals("")) {
+//                filaCosteo.actualizarCosto();
+            } else {
+                System.out.println(errores);
             }
         }
     }
---
+++ src/test/java/excel/CosteoTest.java
@@ -0,0 +1,34 @@
+package excel;
+
+import java.io.File;
+import java.io.IOException;
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class CosteoTest {
+
+    File archivoPlanilla;
+    String rutaArchivo = "src/test/resources/costeo_febrero.xls";
+
+    @Before
+    public void inicializar() {
+        archivoPlanilla = new File(rutaArchivo);
+    }
+
+    @Test
+    public void test() {
+        try {
+            Costeo.importar(archivoPlanilla);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (EncryptedDocumentException e) {
+            System.out.println("El archivo está protegido por contraseña");
+        } catch (InvalidFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
