ea8994cc39acd628516d42cdc76eee28654e5e91
leomarazzo <leonardojmarazzo@gmail.com>
2018-06-09 00:41:33 -0300

    Agregacion de estados docentes Retiro, Renuncia, Fallecido, Licencia

---
+++ sql/EstadoDocentesFaltantes.sql
@@ -0,0 +1,4 @@
+insert into EstadosDocentes (id, Descripcion) values (3, 'Retiro');
+insert into EstadosDocentes (id, Descripcion) values (4, 'Renuncia');
+insert into EstadosDocentes (id, Descripcion) values (5, 'Fallecido');
+insert into EstadosDocentes (id, Descripcion) values (6, 'Licencia');
