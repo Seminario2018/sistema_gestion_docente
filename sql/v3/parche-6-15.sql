091331a288337921959d15f40efbb4b9cc61b9ab
leomarazzo <leonardojmarazzo@gmail.com>
2018-06-15 20:20:03 -0300

    Correcciones BD

--- sql/v3/parche-6-15.sql
+++ sql/v3/parche-6-15.sql
@@ -5,3 +5,7 @@ ADD PRIMARY KEY (`Legajo`, `Cargo`, `FechaUltimoCosto`);
 
 ALTER TABLE `costos` 
 CHANGE COLUMN `idcostos` `id` INT(11) NOT NULL ;
+
+ALTER TABLE `plumasdocentes`.`cargosfaltantes` 
+CHANGE COLUMN `UltimoCosto` `UltimoCosto` FLOAT NULL DEFAULT NULL ,
+CHANGE COLUMN `FechaUltimoCosto` `FechaUltimoCosto` DATE NOT NULL ;
