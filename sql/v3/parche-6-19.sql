9716959bc4d070b3510e1f7948cf452d81580ed5
Tomás <tomasjuran96@gmail.com>
2018-06-21 17:20:58 -0300

    Borrar y Actualizar domicilios y contactos en cascada

--- sql/v3/parche-6-19.sql
+++ sql/v3/parche-6-19.sql
@@ -5,3 +5,22 @@ ALTER TABLE `proyectos`
 CHANGE COLUMN `Nombre` `Nombre` VARCHAR(500) NOT NULL ,
 CHANGE COLUMN `Resumen` `Resumen` VARCHAR(10000) NULL DEFAULT NULL ,
 CHANGE COLUMN `Descripcion` `Descripcion` VARCHAR(1000) NULL DEFAULT NULL ;
+
+ALTER TABLE `contactos` 
+DROP FOREIGN KEY `fk_Contactos_1`;
+ALTER TABLE `contactos` 
+ADD CONSTRAINT `fk_Contactos_1`
+  FOREIGN KEY (`TipoDocumento` , `NroDocumento`)
+  REFERENCES `personas` (`TipoDocumento` , `NroDocumento`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE `domicilios` 
+DROP FOREIGN KEY `fk_domicilios_1`;
+ALTER TABLE `domicilios` 
+ADD CONSTRAINT `fk_domicilios_1`
+  FOREIGN KEY (`TipoDocumento` , `NroDocumento`)
+  REFERENCES `personas` (`TipoDocumento` , `NroDocumento`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
